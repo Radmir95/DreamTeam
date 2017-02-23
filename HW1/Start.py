3ce69d02a6bed184c5df9be8fb1a90a845545f8f
Husnutdinov Radmir <XTreme95@yandex.ru>
2017-02-23 19:51:44 +0300

    Imrove code structure

--- HW1/Start.py
+++ HW1/Start.py
@@ -1,59 +1,56 @@
 import cv2
-import matplotlib.pyplot as plt 
 import numpy as np
+import matplotlib.pyplot as plt 
 
 xAxis = 0
 yAxis = 1
+path_to_image = './images/lena_color_512.tif'
 
-image = cv2.imread('./images/lena_color_512.tif', cv2.IMREAD_GRAYSCALE)
 
-imageArray = np.uint8(image)
+grayImage = cv2.imread(path_to_image, cv2.IMREAD_GRAYSCALE)
+npGrayImage = np.uint8(grayImage)
 
-print(imageArray)
 
-print(image.shape)
-
-mean = np.mean(imageArray)
-
-variance = np.var(imageArray)
-
-median = np.median(imageArray)
+mean = np.mean(npGrayImage)
+variance = np.var(npGrayImage)
+median = np.median(npGrayImage)
 
 print('Mean: ', mean)
 print('Variance: ', variance)
 print('Median: ', median)
 
-binarizationArray = np.zeros(imageArray.shape)
 
-for xPx in range(imageArray.shape[xAxis]):
-    for yPx in range(imageArray.shape[yAxis]):
-        if imageArray[xPx, yPx] > median:
-            binarizationArray[xPx][yPx] = 1
+binarizationImage = np.zeros(npGrayImage.shape)
+
+for xPx in range(npGrayImage.shape[xAxis]):
+    for yPx in range(npGrayImage.shape[yAxis]):
+        if npGrayImage[xPx, yPx] > median:
+            binarizationImage[xPx][yPx] = 1
         else:
-            binarizationArray[xPx][yPx] = 0
+            binarizationImage[xPx][yPx] = 0
                                  
-print(binarizationArray)
 
-cv2.imshow('Image', binarizationArray)
-
+cv2.imshow('Binarization Image', binarizationImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-#plt.hist(imageArray,bins=5)
-#plt.show()
+
+plt.xlabel('Histogram')
+plt.hist(npGrayImage,bins=5)
+plt.show()
 
 
-intValue = np.zeros(imageArray.shape)
+integralImage = np.zeros(npGrayImage.shape)
 
-for i in range(imageArray.shape[xAxis]):
+for i in range(npGrayImage.shape[xAxis]):
     sum = 0
-    for j in range(imageArray.shape[yAxis]):
-        sum+= imageArray[i][j]
+    for j in range(npGrayImage.shape[yAxis]):
+        sum+= npGrayImage[i][j]
         if (i == 0):
-            intValue[i][j] = sum
+            integralImage[i][j] = sum
         else:
-            intValue[i][j] = intValue[i-1][j] + sum
+            integralImage[i][j] = integralImage[i-1][j] + sum
                     
-cv2.imshow('Image', intValue)
+cv2.imshow('Integral Image', integralImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
