3c9756b8535ba2719245aa4642fe316ec2faa842
Husnutdinov Radmir <XTreme95@yandex.ru>
2017-02-23 15:25:53 +0300

    second part of HW1 first version

--- HW1/Start.py
+++ HW1/Start.py
@@ -1,8 +1,43 @@
 import cv2
+import matplotlib.pyplot as plt 
+import numpy as np
 
-image = cv2.imread('C:/Users/Radmir.XTREME95/Desktop/ComputerVision/HW1/images/lena_color_512.tif', -1)
+xAxis = 0
+yAxis = 1
 
-cv2.imshow('Image', image)
+image = cv2.imread('./images/lena_color_512.tif', cv2.IMREAD_GRAYSCALE)
+
+imageArray = np.uint8(image)
+
+print(imageArray)
+
+print(image.shape)
+
+mean = np.mean(imageArray)
+
+variance = np.var(imageArray)
+
+median = np.median(imageArray)
+
+print('Mean: ', mean)
+print('Variance: ', variance)
+print('Median: ', median)
+
+binarizationArray = np.zeros(imageArray.shape)
+
+for xPx in range(imageArray.shape[xAxis]):
+    for yPx in range(imageArray.shape[yAxis]):
+        if imageArray[xPx, yPx] > median:
+            binarizationArray[xPx][yPx] = 1
+        else:
+            binarizationArray[xPx][yPx] = 0
+                                 
+print(binarizationArray)
+
+cv2.imshow('Image', binarizationArray)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+plt.hist(imageArray,bins=5)
+plt.show()
